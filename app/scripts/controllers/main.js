e75bbc5c2b15aa781394a67bb9aedb398a958b42
Nishan Gunawardena <nishan.nilanka@gmail.com>
2015-06-05 05:58:14 +0530

    Remove slider JS and modify the home page using cards

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -31,56 +31,20 @@ if (x==null || x=="")
   alert("Please fill out the username");
   return false;
   }
-}
-
-  function render() {
-    gapi.signin.render('customBtn', {
-      //'callback': 'signinCallback',
-      'clientid': '841077041629.apps.googleusercontent.com',
-      'cookiepolicy': 'single_host_origin',
-      'requestvisibleactions': 'http://schema.org/AddAction',
-      'scope': 'https://www.googleapis.com/auth/plus.login'
-    });
-  }
-
-function slider() {
-
-    function animate_slider(){
-        $('.slider #'+shown).animate({
-            opacity:0 // fade out
-        },1000);
-        $('.slider #'+next_slide).animate({
-            opacity:1.0 // fade in
-        },1000);
-        shown = next_slide;
-    }
-
-    function choose_next() {
-        next_slide = (shown == sc)? 1:shown+1;
-        animate_slider();
-    }
-
-    $('.slider #1').css({opacity:1}); //show 1st image
-    var shown = 1;
-    var next_slide;
-    var sc = $('.slider img').length; // total images
-    var iv = setInterval(choose_next,3500);
-    $('.slider_nav').hover(function(){
-        clearInterval(iv); // stop animation
-    }, function() {
-        iv = setInterval(choose_next,3500); // resume animation
-    });
-    $('.slider_nav span').click(function(e){
-        var n = e.target.getAttribute('name');
-        if (n=='prev') {
-            next_slide = (shown == 1)? sc:shown-1;
-        } else if(n=='next') {
-            next_slide = (shown == sc)? 1:shown+1;
-        } else {
-            return;
+  
+else{
+	 var un = document.forms["login"]["username"].value;
+        var pw = document.forms["login"]["password"].value;
+        var username = "username"; 
+        var password = "password";
+        if ((un == username) && (pw == password)) {
+            return true;
         }
-        animate_slider();
-    });
+        else {
+            alert ("Login was unsuccessful, please check your username and password");
+            return false;
+        }
+}
 }
 
-window.onload = slider;
+
