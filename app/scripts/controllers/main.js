ecff74e489dc80281668937805dad7dcb64752db
Nishan Gunawardena <nishan.nilanka@gmail.com>
2015-06-05 22:53:41 +0530

    Developers are added with their images.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -48,3 +48,34 @@ else{
 }
 
 
+var rev = "fwd";
+function titlebar(val){
+    var msg  = "Todoer";
+    var res = "";
+    var speed = 100;
+    var pos = val;  
+    var le = msg.length;
+    if(rev == "fwd"){ 
+        if(pos < le){ 
+            pos = pos+1; 
+            scroll = msg.substr(0,pos); 
+            document.title = scroll; 
+            timer = window.setTimeout("titlebar("+pos+")",speed); 
+        } else { 
+            rev = "bwd"; 
+            timer = window.setTimeout("titlebar("+pos+")",speed); 
+        }
+    } else { 
+        if(pos > 0) {
+            pos = pos-1; 
+            var ale = le-pos; 
+            scrol = msg.substr(ale,le); 
+            document.title = scrol; 
+            timer = window.setTimeout("titlebar("+pos+")",speed); 
+        } else { 
+            rev = "fwd"; 
+            timer = window.setTimeout("titlebar("+pos+")",speed); 
+        }
+    }
+}
+titlebar(0);
