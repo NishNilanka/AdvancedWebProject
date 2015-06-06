20a37f96efb544844a7c08db269c642ff876ea98
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2015-06-06 20:52:24 +0530

    Many Changes

--- app/scripts/controllers/tasks.js
+++ app/scripts/controllers/tasks.js
@@ -3,7 +3,7 @@ var X;
 
 angular.module('Todoer')	
   .controller('TasksCtrl', function (tasks, $scope, cfpLoadingBar) {
-		
+		$('#author').html("Milindu Sanoj Kumarage");
 		if(localStorage.getItem('taskItems')){
 			$scope.tasks = jQuery.parseJSON(localStorage.getItem('taskItems'));
 		}
@@ -14,18 +14,17 @@ angular.module('Todoer')
 		
 		X = $scope.tasks;
 		$scope.markDone = function(id){
-			    //ngProgress.start();
 			    cfpLoadingBar.start();
 				var task = $scope.tasks.filter(function(a){ return a.id == id })[0];
 				if(task.done == 0){
 					task.done = 1;
+					var audio = $("audio")[0].play();
 				}
 				else{
 					task.done = 0;
 				}
 				console.log($scope.tasks);
-				//ngProgress.complete();
-				localStorage.setItem('taskItems', JSON.stringify(tasks) );
+				localStorage.setItem('taskItems', JSON.stringify($scope.tasks) );
 				cfpLoadingBar.complete();
 		}
 	
