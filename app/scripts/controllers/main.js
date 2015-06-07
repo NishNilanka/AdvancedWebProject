8c3f5664834a7b31c24f800612ee379d987a8b32
Nishan Gunawardena <nishan.nilanka@gmail.com>
2015-06-07 15:32:39 +0530

    Facebook button added

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -91,14 +91,17 @@ $(document).ready(function() {
 	  });
 
 });
-<<<<<<< HEAD
 
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-=======
->>>>>>> 638ae82784011495a01d3c99d74e6cb632b29868
+$(function(){
+            
+            $("#my_image").hover(function(){
+                $(this).addClass("blinking");
+            }, function() {
+                $(this).removeClass("blinking");
+            }); 
+            
+            $("#hover_blink").addClass("blinking");
+            
+             
+        });
+
